93fa7388dee1924c94d6e1c95a6a07884e72c27c
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2021-04-01 09:22:45 +0530

    rm dist

--- dist/ts/index.d.ts
+++
@@ -1,7 +0,0 @@
-import TajModule from "mahal";
-import { Plugin, App } from "mahal";
-export default class implements Plugin {
-    setup(Taj: typeof TajModule, app: App): {
-        click: () => void;
-    };
-}
--- dist/ts/plugin.d.ts
+++
@@ -1,3 +0,0 @@
-export declare class TajStorePlugin {
-    setup(component: any, store: any): void;
-}
